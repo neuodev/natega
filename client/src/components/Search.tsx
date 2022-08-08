eca477361614c7e523f0963766845d2d0b58c32a
Ahmed Ibrahim <ib153507@gmail.com>
2022-08-08 18:21:26 +0200

    Display results

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -6,15 +6,25 @@ import {
   CircularProgress,
   Container,
   createTheme,
+  Paper,
   Stack,
   styled,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
   TextField,
   ThemeProvider,
   Typography,
 } from "@mui/material";
 import { Box } from "@mui/system";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { SERVER } from "../constants";
 
 type Result = {
   name: string;
@@ -44,48 +54,131 @@ type Result = {
   percentage: string;
 };
 
-const SERVER = "http://64.227.26.187:8000/api/v1";
+const labels = [
+  "الاسم",
+  "المدرسه",
+  "الجهه/ اليئهه",
+  "الحاله",
+  "نوعية التعليم",
+  "الفرع",
+  "اللغه العربيه",
+  "اللغه الاول",
+  "اللغه الثانيه",
+  "الرياضه اليحته",
+  "الرياضه التطبيقيه",
+  "التاريخ",
+  "الجغرفيا",
+  "الفلسفه",
+  "النفسيه",
+  "الكيمياء",
+  "الأحياء",
+  "الجيولوجيا وعلوم البيئة",
+  "الفيزياء",
+  "مجموع الدرجات",
+  "التربية الدينية",
+  "التربية الوطنية",
+  "الاقتصاد والإحصاء",
+  "رقم الجلوس",
+  "النسبه",
+];
 
 function Search() {
-  let [results, setResults] = useState<Result[]>([]);
+  const [seatNo, setSeatNo] = useState<number | null>(null);
+  let [result, setResult] = useState<{
+    res: Result;
+    rank: { resultRank: number; total: number };
+  } | null>(null);
   let [loading, setLoading] = useState<boolean>(false);
   let [error, setError] = useState<null | string>(null);
 
-  useEffect(() => {
-    (async () => {
-      // setLoading(true);
-      // try {
-      //   const res = await Promise.all(
-      //     files.map((f) => axios.get(`${SERVER}/static/${f}`))
-      //   );
-      //   const all: Result[] = [];
-      //   res.forEach((resp) => {
-      //     all.push(...resp.data);
-      //   });
-      //   console.log(all);
-      // } catch (error) {
-      //   const err = error as Error;
-      //   setError(err.message);
-      // }
-      // setLoading(false);
-    })();
-  }, []);
+  const search = async () => {
+    if (!seatNo) return;
+    setLoading(true);
+    try {
+      const [{ data }, { data: rankData }] = await Promise.all([
+        axios.get(`${SERVER}/search?seatNo=${seatNo}`),
+        axios.get(`${SERVER}/rank?seatNo=${seatNo}`),
+      ]);
+      setResult({ res: data, rank: rankData.rank[0] });
+      setError(null);
+    } catch (error) {
+      const err = error as any;
+      setError(
+        err.response?.data?.message ? err.response.data.message : err.message
+      );
+    }
+    setLoading(false);
+  };
+  console.log({ result });
   return (
-    <Box sx={{ mt: "68px", height: "100vh" }}>
-      <Box
+    <Box sx={{ mt: "100px" }}>
+      <Container
+        maxWidth="sm"
         sx={{
-          height: "400px",
+          height: "300px",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
+          flexDirection: "column",
         }}
       >
-        <TextField
-          sx={{ minWidth: "400px" }}
-          label="Seat Number"
-          helperText="Ex: 612198"
-        />
-      </Box>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "start",
+            justifyContent: "center",
+            width: "100%",
+          }}
+        >
+          <TextField
+            value={seatNo || ""}
+            onChange={(e) => {
+              let val = Number(e.target.value);
+              if (isNaN(val) === true) return;
+              setSeatNo(val);
+            }}
+            sx={{ width: "100%", mr: "15px" }}
+            label="Seat Number"
+            error={error !== null}
+            helperText={error === null ? "Ex: 612198" : error}
+          />
+          <Button
+            onClick={search}
+            disabled={!seatNo}
+            sx={{ height: "55px", width: "120px" }}
+            variant="outlined"
+          >
+            Search
+          </Button>
+        </Box>
+
+        <Stack direction="row" sx={{ width: "100%" }}>
+          <Button
+            disabled={!seatNo}
+            onClick={() => {
+              if (!seatNo) return;
+              setSeatNo(seatNo - 1);
+              search();
+            }}
+            fullWidth
+            startIcon={<NavigateBeforeIcon />}
+          >
+            Previous
+          </Button>
+          <Button
+            onClick={() => {
+              if (!seatNo) return;
+              setSeatNo(seatNo + 1);
+              search();
+            }}
+            disabled={!seatNo}
+            fullWidth
+            endIcon={<NavigateNextIcon />}
+          >
+            Next
+          </Button>
+        </Stack>
+      </Container>
       {loading ? (
         <Centered>
           <CircularProgress />
@@ -97,15 +190,64 @@ function Search() {
             Fetching data for students...
           </Typography>
         </Centered>
-      ) : error !== null ? (
-        <Centered>
-          <Alert severity="error">
-            <AlertTitle>Error!</AlertTitle>
-            {error}
-          </Alert>
-        </Centered>
       ) : (
-        <Box></Box>
+        error === null &&
+        result !== null && (
+          <Container
+            maxWidth="sm"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+            }}
+          >
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              sx={{ width: "100%", mb: "24px" }}
+            >
+              <Box sx={{ justifySelf: "flex-start" }}>
+                <Typography variant="h1">
+                  {result.rank ? result.rank.resultRank : "-"}
+                  <Typography variant="subtitle1" component="span">
+                    th
+                  </Typography>
+                </Typography>
+                <Typography>الترتيب ع مستوي الجمهوريه</Typography>
+              </Box>
+              <Box sx={{ justifySelf: "flex-end" }}>
+                <Typography variant="h1">
+                  {result.res.total}
+                  <Typography variant="subtitle1" component="span">
+                    th
+                  </Typography>
+                </Typography>
+                <Typography sx={{ textAlign: "right" }}>المجموع</Typography>
+              </Box>
+            </Stack>
+            <TableContainer sx={{ maxWidth: "sm" }} component={Paper}>
+              <Table size="small" aria-label="a dense table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="right">النتيجه</TableCell>
+                    <TableCell>العنوان</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {Object.values(result.res).map((val, idx) => (
+                    <TableRow>
+                      <TableCell scope="row" align="right">
+                        {val}
+                      </TableCell>
+                      <TableCell>{labels[idx]}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Container>
+        )
       )}
     </Box>
   );
